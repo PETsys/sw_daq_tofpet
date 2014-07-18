fd4b206a9efe08f8b0a22062fe3e8657ddc17c2e
Ricardo Bugalho <ricardo@lip.pt>
2014-07-18 19:23:04 +0100

    Minimal code base version - Only minimal set of scripts - Scripts to be changed renamed to script.template

--- aDAQ/sDraw1D.C
+++
@@ -1,8 +0,0 @@
-{
-	gStyle->SetPalette(1);
-	TGraph *g1 = new TGraph("sptr.txt", "%*lg %lg %*lg %*lg %lg %*lg %*lg %*lg");
-	g1->GetXaxis()->SetTitle("praedictio_delay");	
-	g1->GetYaxis()->SetTitle("MPTR sigma (s)");	
-	g1->Draw("A*");
-
-}
