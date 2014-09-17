b12da130b40136c1bfd68b94174e5debb05edd78
TOFPET Common <tofpet@tagus21.tagus.lip.pt>
2014-09-17 14:28:36 +0100

    Changed FrameServer* and UDPFrameServer* to make it more general and ready for DAQFrameServer

--- daqd/FrameServer.hpp
+++ daqd/FrameServer.hpp
@@ -6,10 +6,14 @@
 
 #include "Protocol.hpp"
 
+static const char *shmObjectPath = "/daqd_shm";
+static const int MaxDataFrameQueueSize = 16*1024;
+static const int N_ASIC=4;
 
 class FrameServer {
 public:
-	virtual ~FrameServer();	
+	FrameServer(int debugLevel);
+        ~FrameServer();	
 	struct DataFramePtr {
 	public:
 		unsigned index;
@@ -19,14 +23,58 @@ public:
 		DataFramePtr (unsigned index, bool fake, DataFrame *p) : index(index), fake(fake), p(p) {};
 	};
 	
+	// Sends a command and gets the reply
+	// buffer is used to carry the command and the reply
+	// bufferSize is the max capacity of the size
+	// commandLength is the length of the command in
+	// return the reply length or -1 if error
 	virtual int sendCommand(char *buffer, int bufferSize, int commandLength) = 0;
+	
+	virtual const char *getDataFrameSharedMemoryName();
+	virtual DataFramePtr *getDataFrameByPtr();
+	virtual void returnDataFramePtr(DataFramePtr *ptr);
+	
+	virtual void startAcquisition(int mode);
+	virtual void stopAcquisition();
+	
+	
+protected:
+	
+	int debugLevel;
+	int dataFrameSharedMemory_fd;
+	DataFrame *dataFrameSharedMemory;
+	
+	static void *runWorker(void *);
+	virtual void * doWork(void *) = 0;
+	static bool decodeDataFrame(FrameServer *m, unsigned char *buffer, int nBytes);
+	void startWorker();
+	void stopWorker();
+	
+	volatile bool die;
+	pthread_t worker;
+	volatile bool hasWorker;
+	volatile int acquisitionMode;
+	pthread_mutex_t lock;
+	pthread_cond_t condCleanDataFrame;
+	pthread_cond_t condDirtyDataFrame;
+	std::queue<DataFramePtr *> cleanDataFrameQueue;
+	std::queue<DataFramePtr *> dirtyDataFrameQueue;
+	
+	
 
-	virtual const char *getDataFrameSharedMemoryName() = 0;
-	virtual DataFramePtr *getDataFrameByPtr() = 0;
-	virtual void returnDataFramePtr(DataFramePtr *ptr) = 0;
+	struct reply_t {
+		int size;
+		char buffer[128];
+	};
+	std::queue<reply_t *> replyQueue;
+	pthread_cond_t condReplyQueue;
+	
+	
+	uint64_t *tacLastEventTime;
+	uint64_t *channelLastEventTime;
+	
 
-	virtual void startAcquisition(int mode) = 0;
-	virtual void stopAcquisition() = 0;
+
 
 };
 
--- daqd/UDPFrameServer.hpp
+++ daqd/UDPFrameServer.hpp
@@ -12,58 +12,19 @@ class UDPFrameServer : public FrameServer
 {
 public:
 	UDPFrameServer(int debugLevel);
-	~UDPFrameServer();	
-
-	const char *getDataFrameSharedMemoryName();
-	DataFramePtr *getDataFrameByPtr();
-	void returnDataFramePtr(DataFramePtr *ptr);
+	virtual ~UDPFrameServer();	
 
 	int sendCommand(char *buffer, int bufferSize, int commandLength);
 	
 
-	
-	void startAcquisition(int mode);
-	void stopAcquisition();
-
 private:
-	int debugLevel;
 	
 	int udpSocket;
-	
-	
-	
-	
-	int dataFrameSharedMemory_fd;
-	DataFrame *dataFrameSharedMemory;
-	
-	static void *runWorker(void *);
-	static bool decodeDataFrame(UDPFrameServer *m, unsigned char *buffer, int nBytes);
-	void startWorker();
-	void stopWorker();
-	
-	volatile bool die;
-	pthread_t worker;
-	volatile bool hasWorker;
-	volatile int acquisitionMode;
-	pthread_mutex_t lock;
-	pthread_cond_t condCleanDataFrame;
-	pthread_cond_t condDirtyDataFrame;
-	std::queue<DataFramePtr *> cleanDataFrameQueue;
-	std::queue<DataFramePtr *> dirtyDataFrameQueue;
-	
-	
 
-	struct reply_t {
-		int size;
-		char buffer[128];
-	};
-	std::queue<reply_t *> replyQueue;
-	pthread_cond_t condReplyQueue;
-	
-	
-	uint64_t *tacLastEventTime;
-	uint64_t *channelLastEventTime;
+protected:
+	void * doWork(void *);
 	
 };
+
 
 #endif
