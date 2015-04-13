e7a61f73d937edae78dc416a964f6c496973390a
TOFPET Common <tofpet@tagus13.tagus.lip.pt>
2015-04-13 10:07:22 +0100

    corrected update_config.template.py

--- update_config.template.py
+++ update_config.template.py
@@ -13,47 +13,47 @@ intToBin = tofpet.intToBin
 atbConfig = atb.BoardConfig()
 atbConfig.asicConfig[0]=AsicConfig()
 atbConfig.asicConfig[1]=AsicConfig()
-atbConfig.asicConfig[4]=AsicConfig()
-atbConfig.asicConfig[5]=AsicConfig()
+atbConfig.asicConfig[2]=AsicConfig()
+atbConfig.asicConfig[3]=AsicConfig()
 
 
 # Global input DC level adjustment
 # Default is 48
-atbConfig.asicConfig[0].globalConfig.setValue("sipm_idac_dcstart", 49)
-atbConfig.asicConfig[1].globalConfig.setValue("sipm_idac_dcstart", 44)
-atbConfig.asicConfig[4].globalConfig.setValue("sipm_idac_dcstart", 48)
-atbConfig.asicConfig[5].globalConfig.setValue("sipm_idac_dcstart", 53)
+atbConfig.asicConfig[0].globalConfig.setValue("sipm_idac_dcstart", 48)
+atbConfig.asicConfig[1].globalConfig.setValue("sipm_idac_dcstart", 48)
+atbConfig.asicConfig[2].globalConfig.setValue("sipm_idac_dcstart", 48)
+atbConfig.asicConfig[3].globalConfig.setValue("sipm_idac_dcstart", 48)
 
 # Channel input DC level adjustment
 # Default is 44
 for channelConfig in atbConfig.asicConfig[0].channelConfig:
-	channelConfig.setValue("vbl", 26)
+	channelConfig.setValue("vbl", 44)
 for channelConfig in atbConfig.asicConfig[1].channelConfig:
-	channelConfig.setValue("vbl", 50)
-for channelConfig in atbConfig.asicConfig[4].channelConfig:
-	channelConfig.setValue("vbl", 32)
-for channelConfig in atbConfig.asicConfig[5].channelConfig:
-	channelConfig.setValue("vbl", 38)
+	channelConfig.setValue("vbl", 44)
+for channelConfig in atbConfig.asicConfig[2].channelConfig:
+	channelConfig.setValue("vbl", 44)
+for channelConfig in atbConfig.asicConfig[3].channelConfig:
+	channelConfig.setValue("vbl", 44)
 
 # Global amplifier reference curerent ib1
 # Default is 24
-atbConfig.asicConfig[0].globalConfig.setValue("vib1", 10)
-atbConfig.asicConfig[1].globalConfig.setValue("vib1", 10)
-atbConfig.asicConfig[4].globalConfig.setValue("vib1", 10)
-atbConfig.asicConfig[5].globalConfig.setValue("vib1", 5)
+atbConfig.asicConfig[0].globalConfig.setValue("vib1", 24)
+atbConfig.asicConfig[1].globalConfig.setValue("vib1", 24)
+atbConfig.asicConfig[2].globalConfig.setValue("vib1", 24)
+atbConfig.asicConfig[3].globalConfig.setValue("vib1", 24)
 
 
 # Amplifier output DC level
 # Default is 50
 atbConfig.asicConfig[0].globalConfig.setValue("postamp", 50)
 atbConfig.asicConfig[1].globalConfig.setValue("postamp", 50)
-atbConfig.asicConfig[4].globalConfig.setValue("postamp", 50)
-atbConfig.asicConfig[5].globalConfig.setValue("postamp", 50)
+atbConfig.asicConfig[2].globalConfig.setValue("postamp", 50)
+atbConfig.asicConfig[3].globalConfig.setValue("postamp", 50)
 
 # Save the configuration into mezzanine/feba specific configuration files
-#dumpAsicConfig(atbConfig, 0, 2, "config/M1/asic.config")
-dumpAsicConfig(atbConfig, 0, 2, "asic68.config")
-dumpAsicConfig(atbConfig, 4, 6, "asic58.config")
+dumpAsicConfig(atbConfig, 0, 2, "config/M1/asic.config")
+dumpAsicConfig(atbConfig, 2, 4, "config/M2/asic.config")
+
 
 # Upload configuration into ATBConfig
 uut = atb.ATB("/tmp/d.sock")
