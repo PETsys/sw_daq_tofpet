76ba0dbaebe4e7de6275203cbdd305e8445bbbf1
Ricardo Bugalho <ricardo@lip.pt>
2015-04-30 15:29:19 +0100

    Update loadLocalConfig template for 64CH mezzanine

--- loadLocalConfig.template_64.py
+++ loadLocalConfig.template_64.py
@@ -6,17 +6,17 @@ from atbUtils import loadAsicConfig, dumpAsicConfig, loadHVDACParams, loadBaseli
 def loadLocalConfig(useBaseline=True):
 	atbConfig = atb.BoardConfig()
 	# HV DAC calibration
-	loadHVDACParams(atbConfig, "config/pab1/hvdac.Config")
+	loadHVDACParams(atbConfig, "config/pab6/hvdac.Config")
 
 	### Mezzanine A (J15) configuration
-	loadAsicConfig(atbConfig, 0, 1, "config/M1/asic.config")
+	loadAsicConfig(atbConfig, 0, 1, "config/FEBA/FEBA07/asic.config")
 	if useBaseline:
-		loadBaseline(atbConfig, 0, 1, "config/M1/asic.baseline");
+		loadBaseline(atbConfig, 0, 1, "config/FEBA/FEBA07/asic.baseline");
 
 	### Mezzanine B (J16) configuration
-	loadAsicConfig(atbConfig, 2, 3, "config/M2/asic.config")
+	loadAsicConfig(atbConfig, 2, 3, "config/FEBA/FEBA14/asic.config")
 	if useBaseline:
-		loadBaseline(atbConfig, 2, 3, "config/M2/asic.baseline");
+		loadBaseline(atbConfig, 2, 3, "config/FEBA/FEBA14/asic.baseline");
 
 
 	return atbConfig
