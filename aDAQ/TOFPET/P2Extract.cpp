32ad1a805c6ab07818f123f883728eed4afb3b2a
Ricardo Bugalho <ricardo@lip.pt>
2015-07-31 15:09:55 +0100

    Attempt to make the best out of events with denormal TDC ranges by ignoring the TDC information for such cases.

--- aDAQ/TOFPET/P2Extract.cpp
+++ aDAQ/TOFPET/P2Extract.cpp
@@ -52,37 +52,16 @@ P2Extract::P2Extract(DAQ::TOFPET::P2 *lut, bool killZeroToT, float tDenormalTole
 // 		atomicAdd(nNotNormal, 1);
 // 		return false;
 // 	}
-		
-	// WARNING: P2::geT() returns time with coarse value already added!
-	float f_T = lut->getT(raw.channelID, raw.d.tofpet.tac, true, tfine, tCoarse, tacIdleTime, coarseToT*raw.T/1000.0) - tCoarse;
-	float f_E = lut->getT(raw.channelID, raw.d.tofpet.tac, false, efine, eCoarse, tacIdleTime, coarseToT*raw.T/1000.0) - eCoarse;
-		
    
 	pulse.raw = raw;		
-	// WARNING: rounding sensitive!
-	pulse.time = raw.time + (long long)((f_T * raw.T)+ lut->timeOffset[raw.channelID]);
-	pulse.timeEnd = raw.timeEnd + (long long)((f_E * raw.T)+ lut->timeOffset[raw.channelID]);
-
 	pulse.region = raw.region;
 	pulse.channelID = raw.channelID;
-		
-	// printf("DBG X1 %f %f %lld %lld %lld %d %d %f\n",
-	// 				     f_T, 
-	// 				     f_E, 
-	// 				     raw.time, raw.timeEnd, 
-	// 				     (pulse.timeEnd - pulse.time)/1000, 
-	// 				     tCoarse,
-	// 				     eCoarse, 
-	// 				     coarseToT*raw.T/1000.0);
-
-	//pulse.energy = 1E-3*(pulse.timeEnd - pulse.time);
-    pulse.energy = lut->getEnergy(raw.channelID, 1E-3*(pulse.timeEnd - pulse.time));
-
-   	//printf("tot=%f energy = %f\n", 1E-3*(pulse.timeEnd - pulse.time), pulse.energy);
-
 
 	pulse.tofpet_TQT = lut->getQ(raw.channelID, raw.d.tofpet.tac, true, tfine, tacIdleTime, coarseToT*raw.T/1000);
 	pulse.tofpet_TQE = lut->getQ(raw.channelID, raw.d.tofpet.tac, false, efine, tacIdleTime, coarseToT*raw.T/1000);
+	// WARNING: P2::geT() returns time with coarse value already added!
+	float f_T = lut->getT(raw.channelID, raw.d.tofpet.tac, true, tfine, tCoarse, tacIdleTime, coarseToT*raw.T/1000.0) - tCoarse;
+	float f_E = lut->getT(raw.channelID, raw.d.tofpet.tac, false, efine, eCoarse, tacIdleTime, coarseToT*raw.T/1000.0) - eCoarse;
 	
 	pulse.badEvent = false;
 	if(pulse.tofpet_TQT < (1.0 - tDenormalTolerance) || pulse.tofpet_TQT > (3.0 + tDenormalTolerance)) {
@@ -90,16 +69,25 @@ P2Extract::P2Extract(DAQ::TOFPET::P2 *lut, bool killZeroToT, float tDenormalTole
 		pulse.badEvent = true;
 		if(killDenormal) return false;
 	}
+	else {
+		// WARNING: rounding sensitive!
+		pulse.time = raw.time + (long long)((f_T * raw.T) + lut->timeOffset[raw.channelID]);
+	}
+	
 	if(pulse.tofpet_TQE < (1.0 - eDenormalTolerance) || pulse.tofpet_TQE > (3.0 + eDenormalTolerance)) {
 		atomicAdd(nNotNormal, 1);
 		pulse.badEvent = true;
 		if(killDenormal) return false;
 	}
+	else {
+		// WARNING: rounding sensitive!
+		pulse.timeEnd = raw.timeEnd + (long long)((f_E * raw.T) + lut->timeOffset[raw.channelID]);
+	}
+   	//printf("tot=%f energy = %f\n", 1E-3*(pulse.timeEnd - pulse.time), pulse.energy);
+	pulse.energy = lut->getEnergy(raw.channelID, 1E-3*(pulse.timeEnd - pulse.time));
 
 	atomicAdd(nPassed, 1); 
-
 	return true; 
-
 }
 
 EventBuffer<Pulse> * P2Extract::handleEvents (EventBuffer<RawPulse> *inBuffer)
