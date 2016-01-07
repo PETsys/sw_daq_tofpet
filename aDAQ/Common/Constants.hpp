2cc5155a2658a491336d3b179c12906ab1ef822b
Ricardo Bugalho <ricardo@lip.pt>
2016-01-07 10:45:30 +0000

    Correct maximum system size in aDAQ/Common/Constants.hpp

--- aDAQ/Common/Constants.hpp
+++ aDAQ/Common/Constants.hpp
@@ -2,7 +2,7 @@
 #define __DAQ__COMMON__CONSTANTS_HPP__DEFINED__
 namespace DAQ { namespace Common {
 	
-	static const int SYSTEM_NCRYSTALS = 4096;
+	static const int SYSTEM_NCRYSTALS = 2*16*16*64;
 	static const int SYSTEM_NCHANNELS = SYSTEM_NCRYSTALS;
 	static const float SYSTEM_PERIOD = 6.25E-9;
 	
